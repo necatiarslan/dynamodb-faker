3a3200775a21b6076b82524dd95533983341a2fe
Necati Arslan <necatia@gmail.com>
2023-11-09 16:34:09 -0500

    cli fix

--- dynamodbfaker/cli.py
+++ dynamodbfaker/cli.py
@@ -4,12 +4,10 @@ from . import dynamodbfaker
 def main():
     parser = argparse.ArgumentParser(description=get_description())
     parser.add_argument('--config', required=True, help='Config yaml file path')
-    parser.add_argument('--file_type', required=False, help='Target file type (csv,json,parquet,excel)')
     parser.add_argument('--target', required=False, help='Target folder/file')
 
     args = parser.parse_args()
 
-    file_type = None
     config_file_path = None
     target_file_path = None
 
@@ -19,11 +17,6 @@ def main():
         print("Missing --config parameter. Use --help for more detail.")
         return
 
-    if args.file_type is not None:
-        file_type = args.file_type
-    else:
-        file_type = "csv"
-
     if args.target is not None:
         target_file_path = args.target
     else:
@@ -31,13 +24,13 @@ def main():
 
 
     if isinstance(config_file_path, str):
-        dynamodbfaker.to_target(file_type, config_file_path, target_file_path)
+        dynamodbfaker.to_target("json", config_file_path, target_file_path)
     else:
         print("Wrong paramater(s)")
         print(get_description())
 
 def get_description():
-    return "more detail: https://github.com/necatiarslan/dynamodb-faker/blob/main/README.md "
+    return "more detail: https://github.com/necatiarslan/dynamodb-faker/blob/main/README.md"
 
 if __name__ == '__main__':
     main()
